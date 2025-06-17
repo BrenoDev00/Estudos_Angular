08cc7402e16972131763373663ab707d6cb67c0f
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-06-17 14:41:07 -0300

    adiciona componente de lista de livros

--- bibliograph/src/app/app.component.ts
+++ bibliograph/src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { BookCardComponent } from './components/book-card/book-card.component';
+import { BookListComponent } from './components/book-list/book-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, BookCardComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, BookListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
