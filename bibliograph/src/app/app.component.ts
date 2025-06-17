91e7193d3581097857ee29a6bccba5c2d8b76a1d
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-06-17 10:37:30 -0300

    adiciona componente book card

--- bibliograph/src/app/app.component.ts
+++ bibliograph/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { BookCardComponent } from './components/book-card/book-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, BookCardComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
