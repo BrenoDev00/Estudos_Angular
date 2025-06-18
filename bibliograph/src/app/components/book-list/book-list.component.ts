2789295c7c189ee036ae0b7b5fbe83ffe14b380e
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-06-18 11:53:55 -0300

    implementa lógica do book list component

--- bibliograph/src/app/components/book-list/book-list.component.ts
+++ bibliograph/src/app/components/book-list/book-list.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookGenderInterface } from '../book-card/book-gender.type';
+import { BookInterface } from '../book-card/book.type';
+import { mockedBooks } from '../../constants/mocked-books';
 
 @Component({
   selector: 'app-book-list',
   imports: [],
   templateUrl: './book-list.component.html',
-  styleUrl: './book-list.component.css'
+  styleUrl: './book-list.component.css',
 })
-export class BookListComponent {
+export class BookListComponent implements OnInit {
+  genders: BookGenderInterface[] = [];
 
+  booksByGender: Map<string, BookInterface[]> = new Map();
+
+  ngOnInit(): void {
+    this.booksByGender = new Map();
+
+    mockedBooks.forEach((book) => {
+      const genderId = book.gender.id;
+
+      if (!this.booksByGender.has(genderId)) {
+        this.booksByGender.set(genderId, []);
+      }
+
+      this.booksByGender.get(genderId)?.push(book);
+    });
+
+    this.genders = [
+      {
+        id: 'romance',
+        value: 'Romance',
+        books: this.booksByGender.get('romance') ?? [],
+      },
+      {
+        id: 'misterio',
+        value: 'Mistério',
+        books: this.booksByGender.get('misterio') ?? [],
+      },
+      {
+        id: 'fantasia',
+        value: 'Fantasia',
+        books: this.booksByGender.get('fantasia') ?? [],
+      },
+      {
+        id: 'ficcao-cientifica',
+        value: 'Ficção Científica',
+        books: this.booksByGender.get('ficcao-cientifica') ?? [],
+      },
+      {
+        id: 'tecnicos',
+        value: 'Técnicos',
+        books: this.booksByGender.get('tecnicos') ?? [],
+      },
+    ];
+
+    console.log(this.genders);
+  }
 }
