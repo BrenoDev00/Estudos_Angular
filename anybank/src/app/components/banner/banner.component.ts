3d004b7d33f4c179648a582da64920565e4809c5
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-06-23 15:28:30 -0300

    aprendendo sobre o data pipe

--- anybank/src/app/components/banner/banner.component.ts
+++ anybank/src/app/components/banner/banner.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-banner',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './banner.component.html',
-  styleUrl: './banner.component.css'
+  styleUrl: './banner.component.css',
 })
 export class BannerComponent {
-
+  currentDate: Date = new Date();
 }
