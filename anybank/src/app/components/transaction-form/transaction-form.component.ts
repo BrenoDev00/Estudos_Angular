0f0ce9b71f96bae02bea72f4a0d2d50fc26a8abe
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-06-24 15:35:46 -0300

    ajustes de property bind

--- anybank/src/app/components/transaction-form/transaction-form.component.ts
+++ anybank/src/app/components/transaction-form/transaction-form.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { SelectFieldComponent } from '../select-field/select-field.component';
 import { ButtonComponent } from '../button/button.component';
 import { InputFieldComponent } from '../input-field/input-field.component';
 
 @Component({
   selector: 'app-transaction-form',
-  imports: [SelectFieldComponent, ButtonComponent, InputFieldComponent],
+  imports: [
+    SelectFieldComponent,
+    ButtonComponent,
+    InputFieldComponent,
+    FormsModule,
+  ],
   templateUrl: './transaction-form.component.html',
   styleUrl: './transaction-form.component.css',
 })
-export class TransactionFormComponent {}
+export class TransactionFormComponent {
+  value: string = '123';
+
+  onSubmit() {
+    alert(`valor: ${this.value}`);
+  }
+}
