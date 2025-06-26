8dd8b59a6a2916eaa98006cba30e55ccbf26efb2
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-06-26 11:25:15 -0300

    adiciona signal reativo

--- anybank/src/app/app.component.ts
+++ anybank/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { BannerComponent } from './components/banner/banner.component';
 import { TransactionFormComponent } from './components/transaction-form/transaction-form.component';
 import { Transaction } from './models/transaction';
@@ -10,7 +10,10 @@ import { Transaction } from './models/transaction';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  transactionList: WritableSignal<Transaction[]> = signal<Transaction[]>([]);
+
   processTransaction(transaction: Transaction): void {
-    alert(transaction.transactionType);
+    this.transactionList.update((currentList) => [transaction, ...currentList]);
+    console.log(this.transactionList());
   }
 }
