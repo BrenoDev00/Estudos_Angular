d4f8050ed2ef27702896811e56e57d8224a1f764
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-06-26 14:13:02 -0300

    calculando o saque

--- anybank/src/app/app.component.ts
+++ anybank/src/app/app.component.ts
@@ -8,6 +8,7 @@ import {
 import { BannerComponent } from './components/banner/banner.component';
 import { TransactionFormComponent } from './components/transaction-form/transaction-form.component';
 import { Transaction } from './models/transaction';
+import { TransactionTypeEnum } from './types/models/transaction.type';
 
 @Component({
   selector: 'app-root',
@@ -20,7 +21,11 @@ export class AppComponent {
 
   balance: Signal<number> = computed(() => {
     return this.transactionList().reduce((acc, currentTransaction) => {
-      return acc + currentTransaction.transactionValue;
+      if (currentTransaction.transactionType === TransactionTypeEnum.Deposito) {
+        return acc + currentTransaction.transactionValue;
+      }
+
+      return acc - currentTransaction.transactionValue;
     }, 0);
   });
 
