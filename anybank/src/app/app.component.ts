bc845b20f93569c69529c0f6e4d172ed6a34c595
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-06-26 13:53:26 -0300

    calculando o saldo

--- anybank/src/app/app.component.ts
+++ anybank/src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component, signal, WritableSignal } from '@angular/core';
+import {
+  Component,
+  signal,
+  WritableSignal,
+  computed,
+  Signal,
+} from '@angular/core';
 import { BannerComponent } from './components/banner/banner.component';
 import { TransactionFormComponent } from './components/transaction-form/transaction-form.component';
 import { Transaction } from './models/transaction';
@@ -12,8 +18,13 @@ import { Transaction } from './models/transaction';
 export class AppComponent {
   transactionList: WritableSignal<Transaction[]> = signal<Transaction[]>([]);
 
+  balance: Signal<number> = computed(() => {
+    return this.transactionList().reduce((acc, currentTransaction) => {
+      return acc + currentTransaction.transactionValue;
+    }, 0);
+  });
+
   processTransaction(transaction: Transaction): void {
     this.transactionList.update((currentList) => [transaction, ...currentList]);
-    console.log(this.transactionList());
   }
 }
