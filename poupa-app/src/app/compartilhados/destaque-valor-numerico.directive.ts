2a98d20e485fcf82082dbc97fb587d051f6bfae3
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-07-01 14:29:05 -0300

    adiciona input em diretiva

--- poupa-app/src/app/compartilhados/destaque-valor-numerico.directive.ts
+++ poupa-app/src/app/compartilhados/destaque-valor-numerico.directive.ts
@@ -1,12 +1,20 @@
-import { Directive, afterRender, ElementRef } from '@angular/core';
+import { Directive, afterRender, input, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appDestaqueValorNumerico]',
 })
 export class DestaqueValorNumericoDirective {
+  appDestaqueValorNumerico = input.required<number>();
+
   constructor(elemento: ElementRef<HTMLElement>) {
     afterRender(() => {
-      elemento.nativeElement.style.color = 'var(--destaque-receita)';
+      if (this.appDestaqueValorNumerico() > 0) {
+        elemento.nativeElement.style.color = 'var(--destaque-receita)';
+      }
+
+      if (this.appDestaqueValorNumerico() < 0) {
+        elemento.nativeElement.style.color = 'var(--destaque-despesa)';
+      }
     });
   }
 }
