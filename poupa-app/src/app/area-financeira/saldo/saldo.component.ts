e56a4f3b3ecb82068baaf6679f406d25cca16e68
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-07-02 10:18:02 -0300

    adiciona componente card personalizável

--- poupa-app/src/app/area-financeira/saldo/saldo.component.ts
+++ poupa-app/src/app/area-financeira/saldo/saldo.component.ts
@@ -1,11 +1,12 @@
 import { CurrencyPipe } from '@angular/common';
 import { Component, input } from '@angular/core';
+import { CardComponent } from '../compartilhados/card/card.component';
 
 @Component({
   selector: 'app-saldo',
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, CardComponent],
   templateUrl: './saldo.component.html',
-  styleUrl: './saldo.component.css'
+  styleUrl: './saldo.component.css',
 })
 export class SaldoComponent {
   saldo = input.required<number>();
