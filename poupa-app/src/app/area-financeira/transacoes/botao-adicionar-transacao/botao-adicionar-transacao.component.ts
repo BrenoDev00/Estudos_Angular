1f7f347b50a3b9f9d6d535074068e93781f46607
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-07-04 11:14:21 -0300

    adiciona form nova transação

--- poupa-app/src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
+++ poupa-app/src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
@@ -1,16 +1,29 @@
 import { Component, signal } from '@angular/core';
 import { BotaoComponent } from '../../../compartilhados/botao/botao.component';
 import { ModalComponent } from '../../../compartilhados/modal/modal.component';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-botao-adicionar-transacao',
-  imports: [BotaoComponent, ModalComponent],
+  imports: [BotaoComponent, ModalComponent, FormsModule],
   templateUrl: './botao-adicionar-transacao.component.html',
   styleUrl: './botao-adicionar-transacao.component.css',
 })
 export class BotaoAdicionarTransacaoComponent {
   modalAberto = signal<boolean>(false);
 
+  novaTransacaoForm = {
+    nome: '',
+    tipo: '',
+    valor: '',
+    data: '',
+    conta: '',
+  };
+
   abrirModal(): void {
     this.modalAberto.set(true);
+  }
+
+  aoSubmeter(): void {
+    console.log(this.novaTransacaoForm);
   }
 }
