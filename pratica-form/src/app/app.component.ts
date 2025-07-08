f8b21c8850926a3f165ab56bea045e40f15c7a86
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-07-08 14:38:43 -0300

    praticando com o setValue

--- pratica-form/src/app/app.component.ts
+++ pratica-form/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormControl } from '@angular/forms';
 import { TextFieldComponent } from './components/text-field/text-field.component';
+import { ButtonComponent } from './components/button/button.component';
 @Component({
   selector: 'app-root',
-  imports: [ReactiveFormsModule, TextFieldComponent],
+  imports: [ReactiveFormsModule, ButtonComponent, TextFieldComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -11,4 +12,12 @@ export class AppComponent {
   nameField = new FormControl('');
 
   ageField = new FormControl(0);
+
+  changeNameField(): void {
+    this.nameField.setValue('Luana');
+  }
+
+  clearNameField(): void {
+    this.nameField.setValue('');
+  }
 }
---
+++ pratica-form/src/app/components/button/button.component.ts
@@ -0,0 +1,13 @@
+import { Component, input, output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css',
+})
+export class ButtonComponent {
+  type = input.required<string>();
+
+  onClick = output<void>();
+}
