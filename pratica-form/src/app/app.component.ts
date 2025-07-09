6a43f2da89f6e60b3a3d81475950c6750636de9e
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-07-09 14:50:55 -0300

    adiciona lógica de form usando o form builder

--- pratica-form/src/app/app.component.ts
+++ pratica-form/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { UserFormComponent } from './components/user-form/user-form.component';
+import { RefactoredUserFormComponent } from './components/refactored-user-form/refactored-user-form.component';
+
 @Component({
   selector: 'app-root',
-  imports: [UserFormComponent],
+  imports: [UserFormComponent, RefactoredUserFormComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
---
+++ pratica-form/src/app/components/refactored-user-form/refactored-user-form.component.ts
@@ -0,0 +1,40 @@
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { TextFieldComponent } from '../text-field/text-field.component';
+import { ButtonComponent } from '../button/button.component';
+@Component({
+  selector: 'app-refactored-user-form',
+  imports: [ReactiveFormsModule, TextFieldComponent, ButtonComponent],
+  templateUrl: './refactored-user-form.component.html',
+})
+export class RefactoredUserFormComponent {
+  private formBuilder = inject(FormBuilder);
+
+  userForm = this.formBuilder.group({
+    name: [''],
+    age: [0],
+    address: this.formBuilder.group({
+      street: [''],
+      city: [''],
+      state: [''],
+    }),
+  });
+
+  onSubmit(): void {
+    alert(JSON.stringify(this.userForm.value));
+
+    console.log(this.userForm.value);
+  }
+
+  resetFields(): void {
+    this.userForm.patchValue({
+      name: '',
+      age: 0,
+      address: {
+        street: '',
+        city: '',
+        state: '',
+      },
+    });
+  }
+}
