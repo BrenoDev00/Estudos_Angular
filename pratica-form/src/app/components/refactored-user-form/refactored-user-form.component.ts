50bc27fb3f3b3484f1fe5d1f9936c4fb4013bf27
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-07-11 10:55:52 -0300

    adiciona validações no refactored form

--- pratica-form/src/app/components/refactored-user-form/refactored-user-form.component.ts
+++ pratica-form/src/app/components/refactored-user-form/refactored-user-form.component.ts
@@ -2,21 +2,52 @@ import { Component, inject } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { TextFieldComponent } from '../text-field/text-field.component';
 import { ButtonComponent } from '../button/button.component';
+import { ErrorMessageComponent } from '../error-message/error-message.component';
 @Component({
   selector: 'app-refactored-user-form',
-  imports: [ReactiveFormsModule, TextFieldComponent, ButtonComponent],
+  imports: [
+    ReactiveFormsModule,
+    ErrorMessageComponent,
+    TextFieldComponent,
+    ButtonComponent,
+  ],
   templateUrl: './refactored-user-form.component.html',
 })
 export class RefactoredUserFormComponent {
   private formBuilder = inject(FormBuilder);
 
   userForm = this.formBuilder.group({
-    name: ['', Validators.required],
-    age: [0],
+    name: [
+      '',
+      [Validators.required, Validators.minLength(3), Validators.maxLength(64)],
+    ],
+    email: ['', [Validators.required, Validators.email]],
+    age: [0, [Validators.required, Validators.min(18), Validators.max(120)]],
     address: this.formBuilder.group({
-      street: [''],
-      city: [''],
-      state: [''],
+      street: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(54),
+        ],
+      ],
+      city: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(54),
+        ],
+      ],
+      state: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(54),
+        ],
+      ],
     }),
   });
 
@@ -29,6 +60,7 @@ export class RefactoredUserFormComponent {
   resetFields(): void {
     this.userForm.patchValue({
       name: '',
+      email: '',
       age: 0,
       address: {
         street: '',
