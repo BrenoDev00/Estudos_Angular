5b488c33a4d09bdf7c9ee466145942df4a748fc5
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-09-09 18:27:41 -0300

    aprendendo a usar routes no angular

--- rotas/src/app/paginas/registro/registro.component.ts
+++ rotas/src/app/paginas/registro/registro.component.ts
@@ -1,22 +1,27 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { inject } from '@angular/core';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
+  styleUrls: ['./registro.component.css'],
 })
 export class RegistroComponent {
   registroForm: FormGroup;
 
-  constructor(
-    private fb: FormBuilder,
-    private router: Router,
-  ) {
+  private readonly router = inject(Router);
+
+  constructor(private fb: FormBuilder) {
     this.registroForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
@@ -25,10 +30,6 @@ export class RegistroComponent {
   }
 
   aoEnviar() {
-    if (this.registroForm.valid) {
-      this.router.navigate(["/posts"])
-    }
+    this.router.navigate(['/posts']);
   }
-
-
 }
