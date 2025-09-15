da7750e7d26a5e973aa101a525d54949241cadaf
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-09-15 19:39:44 -0300

    configura página not found

--- rotas/src/app/app.routes.ts
+++ rotas/src/app/app.routes.ts
@@ -2,15 +2,23 @@ import { Routes } from '@angular/router';
 import { RegistroComponent } from './paginas/registro/registro.component';
 import { ListaPostagemComponent } from './paginas/lista-postagem/lista-postagem.component';
 import { AuthGuard } from './guards/auth.guard';
+import { NaoEncontradoComponent } from './paginas/nao-encontrado/nao-encontrado.component';
 
 export const routes: Routes = [
   {
     path: 'registro',
+    title: 'Registro',
     component: RegistroComponent,
   },
   {
     path: 'posts',
+    title: 'Posts',
     canActivate: [AuthGuard],
     component: ListaPostagemComponent,
+  },
+  {
+    path: '**',
+    title: 'Página não encontrada',
+    component: NaoEncontradoComponent,
   },
 ];
--- rotas/src/app/paginas/nao-encontrado/nao-encontrado.component.ts
+++ rotas/src/app/paginas/nao-encontrado/nao-encontrado.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-nao-encontrado',
   imports: [],
   templateUrl: './nao-encontrado.component.html',
-  styleUrl: './nao-encontrado.component.css'
+  styleUrl: './nao-encontrado.component.css',
 })
 export class NaoEncontradoComponent {
+  private readonly router = inject(Router);
 
+  redirectToRegisterPage(): void {
+    this.router.navigate(['/registro']);
+  }
 }
