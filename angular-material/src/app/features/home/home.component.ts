6dbc2810a8010b7a467875e835f129b2e4525d2b
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-11-21 18:52:04 -0300

    desenvolvimento inicial de formulário

--- angular-material/src/app/features/home/home.component.ts
+++ angular-material/src/app/features/home/home.component.ts
@@ -1,11 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 @Component({
   selector: 'home',
-  imports: [],
+  imports: [MatButtonToggleModule],
   templateUrl: './home.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
