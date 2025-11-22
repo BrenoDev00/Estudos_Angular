dde586c269de08d31ed98950bdd574366d1f2f1e
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-11-21 21:17:42 -0300

    finaliza curso

--- angular-material/src/app/features/home/home.component.ts
+++ angular-material/src/app/features/home/home.component.ts
@@ -5,6 +5,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'home',
@@ -14,12 +15,33 @@ import { MatButtonModule } from '@angular/material/button';
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './home.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
   private readonly authService = inject(AuthService);
+  private readonly fb = inject(FormBuilder);
 
   readonly isLoggedIn = this.authService.isLoggedIn;
+
+  readonly flightForm = this.fb.group({
+    origin: ['', Validators.required],
+    destination: ['', Validators.required],
+    ticketType: ['', Validators.required],
+  });
+
+  readonly formControls = this.flightForm.controls;
+
+  toggleFlightFormFields = (): void => {
+    this.flightForm.patchValue({
+      origin: this.formControls.destination.value,
+      destination: this.formControls.origin.value,
+    });
+  };
+
+  handleFormSubmit = (): void => {
+    alert(JSON.stringify(this.flightForm.value));
+  };
 }
